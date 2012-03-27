f61675a0d8005c8c968fdfaa1ab84bee39dbd3aa
kawasima <kawasima1016@gmail.com>
2012-03-27 23:58:50 +0900

    Fixed #1

--- src/main/java/net/unit8/taglib/assets/JavascriptTag.java
+++ src/main/java/net/unit8/taglib/assets/JavascriptTag.java
@@ -7,16 +7,45 @@ import javax.servlet.jsp.JspWriter;
 public class JavascriptTag extends AssetsBaseTag {
 
 	private static final long serialVersionUID = 1L;
+	private Boolean defer = false;
+	private Boolean async = false;
+	private String charset;
 
-	@Override
-	protected String getExtension() {
-		return "js";
-	}
 	@Override
 	protected void writeTag(String path) throws IOException {
 		JspWriter writer = (bodyContent != null) ? bodyContent.getEnclosingWriter() : pageContext.getOut();
 		writer.print("<script type=\"text/javascript\" src=\""
-				+ getAssetsPrefix() + path + "\"></script>");
+				+ getAssetsPrefix() + path + "\"");
+		if (charset != null)
+			writer.print(" charset=\"" + charset + "\"");
+		if (defer)
+			writer.print(" defer");
+		if (async)
+			writer.print(" async");
+		writer.print("></script>");
+	}
+	@Override
+	protected String getExtension() {
+		return "js";
+	}
+
+	public Boolean getDefer() {
+		return defer;
+	}
+	public void setDefer(Boolean defer) {
+		this.defer = defer;
+	}
+	public Boolean getAsync() {
+		return async;
+	}
+	public void setAsync(Boolean async) {
+		this.async = async;
+	}
+	public String getCharset() {
+		return charset;
+	}
+	public void setCharset(String charset) {
+		this.charset = charset;
 	}
 
 }
--- src/main/java/net/unit8/taglib/assets/StylesheetTag.java
+++ src/main/java/net/unit8/taglib/assets/StylesheetTag.java
@@ -8,6 +8,14 @@ public class StylesheetTag extends AssetsBaseTag {
 
 	private static final long serialVersionUID = 1L;
 
+	private String media = null;
+
+	public String getMedia() {
+		return media;
+	}
+	public void setMedia(String media) {
+		this.media = media;
+	}
 	@Override
 	protected String getExtension() {
 		return "css";
@@ -16,7 +24,10 @@ public class StylesheetTag extends AssetsBaseTag {
 	protected void writeTag(String path) throws IOException {
 		JspWriter writer = (bodyContent != null) ? bodyContent.getEnclosingWriter() : pageContext.getOut();
 		writer.print("<link rel=\"stylesheet\" type=\"text/css\" href=\""
-				+ getAssetsPrefix() + path + "\" />");
+				+ getAssetsPrefix() + path + "\"");
+		if (media != null)
+			writer.print(" media=\"" + media + "\"");
+		writer.print(" />");
 	}
 
 }
